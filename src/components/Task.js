fa3446f7a1038026f712efc2d2624c64ea91e5ef
Charlie <ctimlock@gmail.com>
2021-12-15 17:22:30 +1100

    51:36

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 const Task = (props) => {
     return (
         <div className='task'>
-            <h3>{props.task.text}</h3>
+            <h3>{props.task.text} <FaTimes style={{color: 'red', cursor:'pointer'}}/></h3>
             <p>{props.task.day}</p>
         </div>
     )
